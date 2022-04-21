a5d30371f989c07624c4dd1c7174c25dd3de2207
JohnSmithW <55023917+JohnSmithW@users.noreply.github.com>
2022-04-21 12:58:16 +0300

    Add files via upload

--- lab9/calc.js
+++ lab9/calc.js
@@ -1,3 +1,21 @@
-const checkSymbol = (value) => {
-    
+'use strict'
+
+
+const checkOperator = (value) => {
+
+}
+
+const calc = (value) => {
+
+  
+  const regExp = /\(([^)]+)\)/g;
+  const matches = value.match(regExp);
+  const result = [];
+  for (let index = 0; index < matches.length; index += 1) {
+    result.push(matches[index].substring(1, matches[index].length - 1));
+  }
+
+ 
+  console.log(result);
+
 }
